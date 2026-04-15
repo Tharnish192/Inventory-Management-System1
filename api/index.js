5d753c5dd0418680f34687fb14889b2a3a97c8af
Tharnish192 <2k25mca34@kiot.ac.in>
2026-04-15 13:12:39 +0530

    Configure json-server for Vercel deployment

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add custom routes if needed
+server.use(router);
+
+module.exports = server;
